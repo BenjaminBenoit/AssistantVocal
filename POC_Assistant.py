c64bf2bf9bf37dd3536377ebf417f7d5ce0e7912
Benjamin <rosabenjamin@gmail.com>
2018-06-10 16:56:00 -0400

    Clean code + Weather command now functional + Update Readme

--- POC_Assistant.py
+++ POC_Assistant.py
@@ -2,7 +2,7 @@
 """
 Created on Mon May 21 20:41:38 2018
 
-@author: Admin
+@author: Benjamin Rosa
 """
 # USEFUL SHORTCUT FOR SPYDER AND OR PYTHON
 # switch from editor to console : ctrl+shift+e // ctrl+shift+i
@@ -10,16 +10,15 @@ Created on Mon May 21 20:41:38 2018
 # comment one line : ctrl+1
 #############################################
 
-# TODO : this file should onlu contain what is necesary to boot the application
+# TODO : this file should only contain what is necesary to boot the application
 # create separate file and class to initialize the engine and bootstrap the app
 # and when time is due : clean the comments
 
-import webbrowser
-import string
-import torch
+
 # Pyaudio API : http://people.csail.mit.edu/hubert/pyaudio/docs/
 import pyaudio
 import pyttsx3
+from selenium import webdriver
 import speech_recognition as sr
 
 # =============================================================================
@@ -35,39 +34,6 @@ print(pyaudio.pa.get_device_count())
 print("=== Index of default input device : ")
 print(pyaudio.pa.get_default_input_device())
 
-
-# obtain audio
-# https://stackoverflow.com/questions/42184876/google-speech-recognition-api-not-listening
-"""
-r = sr.Recognizer()
-with sr.AudioFile("microphone-results.wav") as source:
-    audio = r.listen(source)
-print("START GOOGLE API")
-print(r.recognize_google(audio))
-print("FINI")
-"""
-
-"""
-r = sr.Recognizer()
-with sr.Microphone(device_index = 1, sample_rate = 48000) as source:
-    audio = r.record(source, duration = 5)
-with open("microphone-results.wav", "wb") as f:
-    f.write(audio.get_wav_data())
-    """
-
-"""
-r = sr.Recognizer()
-with sr.Microphone(1) as source:
-   print("Hello what can i help you find?")
-   audio = r.listen(source)
-   print("OK")
-   
-print(r.recognize_google(audio))
-print("test print")
-"""
-
-#!/usr/bin/env python3
-
 # NOTE: this example requires PyAudio because it uses the Microphone class
 
 # obtain audio from the microphone
@@ -78,27 +44,20 @@ r = sr.Recognizer()
 # because he will think that the user is not done speaking (because of the noise)
 # The way to overcome this issue is to set the dynamic energy threshold at false
 # and to put the energy threshold at 400 (anythong less than that won't work)
-# IMPORTANT : if not using a microphone, those 2 lines need to be uncomment
+# !!!!!!!!!!!!!!!! IMPORTANT !!!!!!!!!!!!!! : if not using a microphone, need to set the dynamic_energy_threshold and the energy_threshold
 #r.dynamic_energy_threshold = False
 #r.energy_threshold = 400
 with sr.Microphone(1) as source:
     print("Say something!")
     audio = r.listen(source, 1)
     print("Sentence recorded")
+    
+def computerSay(sentenceToBeSaid):
+        engine.say(sentenceToBeSaid)
+        engine.runAndWait()
+        engine.stop()    
 
-
-
-# recognize speech using Sphinx
-""" Pocket sphynx is not working at the moment
-try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
-"""
-
-# recognize speech using Google Speech Recognition
+# Recognize speech using Google Speech Recognition
 try:
     # For windows, the driver is sapi5. It's nsss for OSX and espeak for other OS
     # Even if optional, need to specify the driver otherwise an error occur
@@ -106,15 +65,14 @@ try:
     #engine = pyttsx3.init(driverName="sapi5")
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
-    for voice in voices:
-        print(voice, voice.id)
+    #for voice in voices:
+        #print(voice, voice.id)
     # By default, the engine voice is the one of the OS (so French for me), since the command are in english, to have a 
     # good pronounciation, it's important to set the engine voice in english
-    # TODO : create a separate file with methods to initialize the engine.
+    # TODO : create a separate file with methods to initialize the engine. the init method
+    # should take a param like EN or FR and set the right voices
     engine.setProperty('voice', "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0")
 
-            
-    
     sentenceSaidByUser = r.recognize_google(audio)
     
     # for testing purposes, we're just using the default API key
@@ -122,69 +80,24 @@ try:
     # instead of `r.recognize_google(audio)`
     print("Google Speech Recognition thinks you said " + sentenceSaidByUser)
     if(sentenceSaidByUser == "weather"):
-        engine.say("You said weather")
-        engine.runAndWait()
-        engine.stop()
-        print("Use scrappy to get the weather")
+        computerSay("Please wait while I'm looking for the current temperature in Montreal")
+        print("Prepare Selenium webdriver to get the weather")
+        
+        # Need to specify the headless option otherwise selenium will open
+        # a chrome window and we don't want that
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        driver = webdriver.Chrome(executable_path=r"C:\Program Files (x86)\Chrome\chromedriver.exe", chrome_options=options)
+        driver.get("https://www.meteomedia.com/ca/meteo/quebec/montreal")
+
+        spanWithTemperature = driver.find_elements_by_xpath('.//span[@class = "temp"]')[0]
+        print(spanWithTemperature.text)
+        
+        computerSay("The temperature is currently " + spanWithTemperature.text + "degree")      
     else:
-        engine.say("I don't understand")
-        engine.runAndWait()
-        engine.stop()
+        computerSay("I don't understand")
         print("I don't understand")
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
-
-
-
-# recognize speech using Google Cloud Speech
-"""
-GOOGLE_CLOUD_SPEECH_CREDENTIALS = INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE
-try:
-    print("Google Cloud Speech thinks you said " + r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
-except sr.UnknownValueError:
-    print("Google Cloud Speech could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Cloud Speech service; {0}".format(e))
-
-# recognize speech using Wit.ai
-WIT_AI_KEY = "INSERT WIT.AI API KEY HERE"  # Wit.ai keys are 32-character uppercase alphanumeric strings
-try:
-    print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
-except sr.UnknownValueError:
-    print("Wit.ai could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Wit.ai service; {0}".format(e))
-
-# recognize speech using Microsoft Bing Voice Recognition
-BING_KEY = "INSERT BING API KEY HERE"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
-
-# recognize speech using Houndify
-HOUNDIFY_CLIENT_ID = "INSERT HOUNDIFY CLIENT ID HERE"  # Houndify client IDs are Base64-encoded strings
-HOUNDIFY_CLIENT_KEY = "INSERT HOUNDIFY CLIENT KEY HERE"  # Houndify client keys are Base64-encoded strings
-try:
-    print("Houndify thinks you said " + r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY))
-except sr.UnknownValueError:
-    print("Houndify could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Houndify service; {0}".format(e))
-
-# recognize speech using IBM Speech to Text
-IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
-try:
-    print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
-except sr.UnknownValueError:
-    print("IBM Speech to Text could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from IBM Speech to Text service; {0}".format(e))
-"""
--- README.md
+++ README.md
@@ -5,22 +5,22 @@
 #### Sprint 1 (30 Mai - 13 Juin)
 * Convertir une commande vocale en texte en utilisant pyaudio et speech_recognition
 * Convertir du texte en son en utilisant pyttsx3
-* Aller chercher la météo du moment en utilisant scrappy
+* Aller chercher la météo du moment en utilisant Selenium
 
 #### Sprint 2 (14 Juin - 27 Juin)
-* Créer une commande pour éteindre l'assistant
 * Créer une réponse si commande n'est pas comprise
-* Créer une commande pour aller chercher la météo
+* Créer une commande pour aller chercher la météo pour une ville donnée
 * Créer une commande pour avoir l'heure dans un pays donné
 * Créer une interface graphique avec 2 boutons : Démarrer et Arrêter, pour envoyer des commandes au besoin à l'assistant
 
 #### Sprint 3 (28 Juin - 11 Juillet)
-* Travailler sur les éventuels defects / amélioration
+* Travailler sur les éventuels defects / amélioration / refactorisation
 * Préparer la présentation à faire en classe
 * Si le temps le permet :
   * Créer de nouvelles commandes (ex: garder en mémoire des rendez-vous et prévenir l'utilisateur d'un rendez-vous imminent)
   * Rendre l'assistant 'résistant' aux variations de mots dans la commande par l'implémentation d'un réseau de neurones
   * Rendre l'assistant capable de faire une recherche par lui même en cas de commande inconnue. En cas de bonne réponse, l'assistant pourrait apprendre cette commande
+  * Transformer l'application en une application Android
 
 ## Set up
 
@@ -28,7 +28,10 @@
 * Installer PyAudio via Anaconda Prompt https://anaconda.org/anaconda/pyaudio
 * Installer pyttsx3 via Anaconda Prompt https://anaconda.org/auto/pyttsx
 * Installer speech_recognition via Anaconda Prompt https://anaconda.org/conda-forge/speechrecognition
-* Install scrappy library using command line https://anaconda.org/anaconda/scrapy
+* Installer Selenium via Anaconda Prompt https://anaconda.org/conda-forge/selenium
+* Télécharger la dernière version de chromedriver https://sites.google.com/a/chromium.org/chromedriver/home et la placer dans C:\Program Files (x86)\Chrome
+TODO : quand le temps le permettra, faire en sorte au moment de bootstrap l'application, de télécharger le chromedriver et de le placer dans le dossier voulu si jamais il n'est pas déjà installé
+
 
 ## Lancer l'application
 
